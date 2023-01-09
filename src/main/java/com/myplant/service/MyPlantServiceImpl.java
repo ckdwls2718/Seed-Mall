03e3f99195c38a4ef0f62fc741fe5decc6ff42b9
ChanGenie <ckdwls0702@naver.com>
2023-01-09 15:10:04 +0900

    마이페이지-식물 - 매퍼, 서비스 작성

--- src/main/java/com/myplant/service/MyPlantServiceImpl.java
+++ src/main/java/com/myplant/service/MyPlantServiceImpl.java
@@ -1,8 +1,53 @@
 package com.myplant.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.myplant.mapper.MyPlantMapper;
+import com.myplant.model.MyPlantVO;
 
 @Service
 public class MyPlantServiceImpl implements MyPlantService {
+	
+	@Autowired
+	MyPlantMapper myplantMapper;
 
+	@Override
+	public int insertMyPlant(MyPlantVO plant) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateMyPlantNickname(MyPlantVO plant) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateMyPlant(MyPlantVO plant) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMyPlant(int plant_idx) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<MyPlantVO> getMyPlantList(int midx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MyPlantVO getMyPlant(int plant_idx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
